414d626a6dad69794581ae63f088d440d311bca4
Santiago Aristizabal <aristizabalsantiago482@gmail.com>
2024-04-18 11:53:12 -0500

    Retornar propiedades con dirección

--- src/services/propiedades.service.ts
+++ src/services/propiedades.service.ts
@@ -3,10 +3,34 @@ import { query } from "../lib/database";
 
 const propiedadService = {
   getAllPropiedades: async (): Promise<unknown> => {
-    return await query("SELECT * FROM properties");
+    const properties = await query("SELECT * FROM properties");
+    const propertiesWithAddress = await Promise.all(
+      properties.map(async (property) => {
+        const direccion = await query(
+          "SELECT calle, numero, ciudad, estado, pais, codpostal FROM addresses WHERE id = $1",
+          [property.direccionid]
+        );
+        return {
+          ...property,
+          direccion: direccion[0],
+        } as Propiedad;
+      })
+    );
+    return propertiesWithAddress;
   },
   getPropiedad: async (id: string) => {
-    return await query("SELECT * FROM properties where id = $1", [id]);
+    const property = await query("SELECT * FROM properties where id = $1", [
+      id,
+    ]);
+    const direccion = await query(
+      "SELECT calle, numero, ciudad, estado, pais, codpostal FROM addresses WHERE id = $1",
+      [property[0].direccionid]
+    );
+
+    return {
+      ...property[0],
+      direccion: direccion[0],
+    } as Propiedad;
   },
   createPropiedad: async (propiedad: Propiedad) => {
     const { calle, numero, ciudad, estado, pais, codigoPostal } =
@@ -19,7 +43,7 @@ const propiedadService = {
       "SELECT id FROM addresses WHERE calle = $1 AND numero = $2 AND estado = $3 AND ciudad = $4",
       [calle, numero, estado, ciudad]
     );
-    const direccionId = getDireccionId[0].id
+    const direccionId = getDireccionId[0].id;
     const {
       usuarioId,
       titulo,
