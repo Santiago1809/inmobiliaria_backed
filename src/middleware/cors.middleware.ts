c4eb35c43453ce865f306dc8c7e97a4d207fe945
Santiago Aristizabal <aristizabalsantiago482@gmail.com>
2024-04-20 18:55:32 -0500

    Fix allow hosts

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -1,7 +1,7 @@
 import type { Request, Response, NextFunction } from "express";
 
 const corsMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4321/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next()
